b2e2dfc7fe5e40627d16a3b68782e339125eb8f9
imJORSA <122283812+imJORSA@users.noreply.github.com>
2023-07-25 21:51:04 +0300

    25.07.2023

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function togglemenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+let allGridItems = [ ...document.getElementsByClassName("image")];
+let popupBg = document.getElementById("popup-bg");
+let popupImg = document.getElementById("popup-img");
+
+const openpopup = (e) => {
+    let gridItemClicked = e.target.closest(".image");
+    let clickedImageName = gridItemClicked.id;
+    popupBg.classList.add("active");
+    popupImg.src = `./assets/3dgallery/img/${clickedImageName}.jpg`;
+}
+
+allGridItems.forEach(el => el.addEventListener("click", openpopup));
